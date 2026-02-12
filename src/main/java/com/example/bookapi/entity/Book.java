ca773f0fbea5bfe1a0415ed9d2e3014161529750
Yoowooyoung <justeenypp@bizspring.co.kr>
2026-02-13 08:47:30 +0900

    Book

--- src/main/java/com/example/bookapi/entity/Book.java
+++ src/main/java/com/example/bookapi/entity/Book.java
@@ -9,57 +9,57 @@ import java.time.LocalDate;
 
 @Entity
 public class Book {
-    @Id // 기본키
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID 자동 생성
-    private Long id;
-    private String title;
-    private String author;
-    private String isbn;
-    private LocalDate publishedDate;
+	@Id // 기본키
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // ID 자동 생성
+	private Long id;
+	private String title;
+	private String author;
+	private String isbn;
+	private LocalDate publishedDate;
 
-    public LocalDate getPublishedDate() {
-        return publishedDate;
-    }
-    public void setPublishedDate(LocalDate publishedDate) {
-        this.publishedDate = publishedDate;
-    }
+	public LocalDate getPublishedDate() {
+		return publishedDate;
+	}
+	public void setPublishedDate(LocalDate publishedDate) {
+		this.publishedDate = publishedDate;
+	}
 
-    public String getIsbn() {
-        return isbn;
-    }
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    @Override
-    public String toString() {
-        return "PostRequestDto {" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", publishedDate='" + publishedDate + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "PostRequestDto {" +
+				"id='" + id + '\'' +
+				", title='" + title + '\'' +
+				", author='" + author + '\'' +
+				", isbn='" + isbn + '\'' +
+				", publishedDate='" + publishedDate + '\'' +
+				'}';
+	}
 }
